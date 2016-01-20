9dfbf0b5ba86426d65c0f812765e315f2f8b039e
Hermanus Wibisono <hermanus_wibisono@yahoo.com>
2016-01-21 00:13:21 +0700

    Complete Chapter 2

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,8 +3,10 @@
     <title> @yield('title') </title>
 
 <link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap.min.css') !!}" >
-<link rel="stylesheet" href="{!! asset('css/bootstrap-theme.min.css') !!}">
-<script src="{!! asset('js/bootstrap.min.js') !!}"></script>
+<!-- <link rel="stylesheet" href="{!! asset('css/bootstrap-theme.min.css') !!}"> -->
+<!-- <script src="{!! asset('js/bootstrap.min.js') !!}"></script> -->
+    <link href="/css/bootstrap-material-design.min.css" rel="stylesheet">
+    <link href="/css/ripples.min.css" rel="stylesheet">
 </head>
 <body>
 
@@ -12,5 +14,15 @@
 
 @yield('content')
 
+<script src="{!! asset('js/bootstrap.min.js') !!}"></script>
+<script src="{!! asset('js/jquery-2.2.0.js') !!}"></script>
+<script src="/js/ripples.min.js"></script>
+<script src="/js/material.min.js"></script>
+<script>
+    $(document).ready(function() {
+        // This command is used to initialize some elements and make them work properly
+        $.material.init();
+    });
+</script>
 </body>
 </html>
