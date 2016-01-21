c6b43b30ed8767a8e13cb0d7df2ebff4fddba906
riopurwanggono <rio.purwanggono@gmail.com>
2016-01-21 20:55:03 +0700

    20160120-02-Rio-solve merge problem

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,19 +5,9 @@
 
 <script src="{!! asset('js/jquery.min.js') !!}"></script>
 <link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap.min.css') !!}" >
-<<<<<<< HEAD
 <link rel="stylesheet" type="text/css" href="{!! asset('css/flat-ui.min.css') !!}" >
 
-
-
-
 <script src="{!! asset('js/bootstrap.min.js') !!}"></script>
-=======
-<!-- <link rel="stylesheet" href="{!! asset('css/bootstrap-theme.min.css') !!}"> -->
-<!-- <script src="{!! asset('js/bootstrap.min.js') !!}"></script> -->
-    <link href="/css/bootstrap-material-design.min.css" rel="stylesheet">
-    <link href="/css/ripples.min.css" rel="stylesheet">
->>>>>>> branch 'master' of https://github.com/riopurwanggono/boombazaar.git
 </head>
 <body>
 
@@ -25,10 +15,6 @@
 
 @yield('content')
 
-<script src="{!! asset('js/bootstrap.min.js') !!}"></script>
-<script src="{!! asset('js/jquery-2.2.0.js') !!}"></script>
-<script src="/js/ripples.min.js"></script>
-<script src="/js/material.min.js"></script>
 <script>
     $(document).ready(function() {
         // This command is used to initialize some elements and make them work properly
--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -14,24 +14,7 @@
         <!-- Navbar Right -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
-<<<<<<< HEAD
                 <li class="active"><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
-                <li>
-						<div class="btn-group">
-            			<button data-toggle="dropdown" class="btn btn-primary dropdown-toggle" type="button">Default <span class="caret"></span></button>
-            			<ul role="menu" class="dropdown-menu">
-              			<li><a href="#">Action</a></li>
-              			<li><a href="#">Another action</a></li>
-              			<li><a href="#">Something else here</a></li>
-              			<li class="divider"></li>
-              			<li><a href="#">Separated link</a></li>
-            			</ul>
-         				 </div><!-- /btn-group -->
-          		</li>
-=======
-                <li class="active"><a href="/home">Home</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/contact">Contact</a></li>
                 <li class="dropdown">
@@ -41,7 +24,6 @@
                         <li><a href="/users/login">Login</a></li>
                     </ul>
                 </li>
->>>>>>> branch 'master' of https://github.com/riopurwanggono/boombazaar.git
             </ul>
         </div>
     </div>
